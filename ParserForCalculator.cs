4c3479b6c3f6d67e0503b75545c70f6dce8e73a1
Samuel <gamingsam77@gmail.com>
2021-02-18 01:55:23 +0000

    Add files via upload

---
+++ ParserForCalculator.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using BinaryTrees;
+
+namespace BinaryTrees
+{
+    public enum Tree
+    {
+        Left,
+        Right
+    }
+
+    public class BinaryTreeOfSyntax
+    {
+        public Node root = null;
+
+        public bool isChildEmpty(Tree treePos)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+            return treePos switch
+            {
+                Tree.Left => root.Left != null,
+                Tree.Right => root.Right != null,
+                _ => false
+            };
+        }
+        public void AddChild(Node child, Tree pos)
+        {
+            var childNode = child;
+            if (root == null)
+            {
+                root = childNode;
+                return;
+            }
+
+            switch (pos)
+            {
+                case Tree.Left:
+                    if (root.Left == null)
+                    {
+                        root.Left = childNode;
+                        break;
+                    }
+                    if (root.Left != null)
+                    {
+                        root.Right = childNode;
+                    }
+
+                    break;
+                case Tree.Right:
+                    root.Right = childNode;
+                    break;
+                default:
+                    Console.WriteLine("Error in Tree Pos");
+                    break;
+            }
+        }
+
+        public void AddRoot(Node Parent, Tree moveTreeTo)
+        {
+            var newParentNode = Parent;
+            if (root == null)
+            {
+                root = newParentNode;
+                return;
+            }
+
+            switch (moveTreeTo)
+            {
+                case Tree.Left:
+                    newParentNode.Left = root;
+                    root = newParentNode;
+                    break;
+                case Tree.Right:
+                    newParentNode.Right = root;
+                    root = newParentNode;
+                    break;
+                default:
+                    Console.WriteLine("There is Error");
+                    break;
+            }
+
+        }
+    }
+
+    public class ParserForCalculator
+    {
+        BinaryTreeOfSyntax SyntaxTree;
+        LinkedListNode<Token> currentToken;
+        
+        public ParserForCalculator(LinkedList<Token> tokens)
+        {
+            var theTokens = tokens;
+            currentToken = tokens.First;
+            SyntaxTree = new BinaryTreeOfSyntax();
+            //theTokens = token;
+            //var SyntaxTree = new BinaryTreeOfSyntax();
+            if (tokens is null)
+            {
+                Console.WriteLine("List is empty");
+                throw new ArgumentNullException(nameof(tokens));
+            }
+
+            var tokLength = theTokens.Count;
+            
+            //-1 because otherwise when it gets to the last item
+            //the next item will be null and the program will crash
+            //Need to fix this in a better way
+            for (var i = 0; i < theTokens.Count; i++)
+            {
+                ParseExpression();
+                var linkedListNode = this.currentToken;
+                if (linkedListNode != null) currentToken = linkedListNode.Next;
+            }
+        }
+
+
+        private LinkedListNode<Token> nextToken()
+        {
+            return currentToken.Next;
+        }
+
+        private void ParseExpression()
+        {
+            if (currentToken.Value.Type == TokenType.Numbers)
+            {
+                ParseNumber();
+            }
+            switch (currentToken.Value.Type)
+            {
+                case TokenType.OpsMult:
+                case TokenType.OpsDiv:
+                case TokenType.OpsSub:
+                case TokenType.OpsAdd:
+                    var ops = new Node();
+                    ops.tokenType = currentToken.Value.Type;
+                    SyntaxTree.AddRoot(ops, Tree.Left);
+                    break;
+            }
+        }
+
+        private void ParseNumber()
+        {
+            if (currentToken.Value.Type == TokenType.Numbers)
+            {
+                var node = new Node
+                {
+                    data = currentToken.Value.value,
+                    tokenType = currentToken.Value.Type
+                };
+                //var nextToken = currentToken.Next;
+                SyntaxTree.AddChild(node, Tree.Left);
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+        //Need a Parse Factor or even more basic that is parse Ops
+        //Ops like Div, Multi, Add, Subtract.
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace BinaryTrees
+{
+    //Basic Noode for the tree
+    // It contains Left, Right Node as pointer
+    //Also has datatype and Tokentype as attributes 
+    public class Node
+    {
+        public string data;
+        public TokenType tokenType;
+        public Node Left  = null;
+        public Node Right = null;
+    }
+
+    //Types of Token There are 
+    public enum TokenType
+    {
+        Numbers,
+        OpsAdd,
+        OpsSub,
+        OpsMult,
+        OpsDiv,
+        None
+    }
+
+    public class Token
+    {
+        public TokenType Type;
+        public String value;
+    }
+
+    public class Tokeinzer
+    {
+        string text;
+
+        public LinkedList<Token> tokenList = new LinkedList<Token>();
+
+        //Tokenizer - Takes in in Input converts them to tokens
+        //token Object
+        public Tokeinzer(string text)
+        {
+            this.text = text;
+            Tokenize();
+        }
+
+
+        private static TokenType Type(char c)
+        {
+            if (!Char.IsDigit(c))
+            {
+                return c switch
+                {
+                    '+' => TokenType.OpsAdd,
+                    '-' => TokenType.OpsSub,
+                    '*' => TokenType.OpsMult,
+                    '/' => TokenType.OpsDiv,
+                    _ => TokenType.None
+                };
+            }
+            return TokenType.Numbers;
+        }
+
+        private void Tokenize()
+        {
+            var toTokenize = text;
+            var buffer = "";
+            for (var i = 0; i < toTokenize.Length; i++)
+            {
+                buffer += toTokenize[i];
+                if (i < toTokenize.Length - 1)
+                {
+                    if (Type(toTokenize[i]) == Type(toTokenize[i + 1])) continue;
+                    var token = new Token { Type = Type(toTokenize[i]), value = buffer };
+                    buffer = "";
+                    //tokenList.Append(token);
+                    tokenList.AddLast(token);
+                }
+                else
+                {
+                    var token = new Token { Type = Type(toTokenize[i]), value = buffer };
+                    //tokenList.Append(token);
+                    tokenList.AddLast(token);
+                }
+            }
+            Console.WriteLine();
+        }
+
+        public void Tokenize2()
+        {
+            var inputToTokenize = text;
+            var tokenTypeTemp = Type(inputToTokenize[0]);
+            var buffer = "";
+
+            for (int i = 0; i < inputToTokenize.Length; i++)
+            {
+                buffer += inputToTokenize[i];
+
+                if (Type(inputToTokenize[i]) != tokenTypeTemp)
+                {
+                    //Create new token for the previous buffer
+                    var token = new Token();
+                    token.Type = tokenTypeTemp;
+                    token.value = buffer;
+
+                    //Add to the List
+                    tokenList.Append(token);
+
+                    //Clean buffer
+                    buffer = Convert.ToString(buffer[buffer.Length - 1]);
+
+                    //Set the tempTokenType to the current token type
+                    tokenTypeTemp = Type(inputToTokenize[i]);
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        //Function that can create an add Expression
+        private static string AddRandomString(int min, int max)
+        {
+            var rand = new Random();
+            var one = Convert.ToString(rand.Next(min, max));
+            var two = Convert.ToString(rand.Next(min, max));
+            
+            //Create a random for the ops 
+            var randValue = rand.Next(0, 4);
+            var stringOfRand = randValue switch
+            {
+                0 => "+",
+                1 => "-",
+                2 => "*",
+                3 => "/",
+                _ => ""
+            };
+            var strExpression = one + stringOfRand + two;
+            return strExpression;
+        }
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Enter Something");
+            //var userInput = "324235+34536";
+            //Console.ReadLine();
+            var testExpression = AddRandomString(1, 10000);
+            var token = new Tokeinzer(testExpression);
+            var tokenList = token.tokenList;
+            var parser = new ParserForCalculator(tokenList);
+            Console.WriteLine("Hopefully This Works");
+        }
+    }
+}
